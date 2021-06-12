50be12b555ee42e12790323ef2daa4463677b544
Blues Young <15171255945@163.com>
2021-06-12 17:37:40 +0800

    feat: :sparkles: 自动脚本加入Linux兼容

--- scripts/new.js
+++ scripts/new.js
@@ -1,9 +1,15 @@
 /*
  * @Author: zhangyang
  * @Date: 2020-12-18 09:34:44
- * @LastEditTime: 2020-12-18 09:37:33
+ * @LastEditTime: 2021-06-12 17:35:22
  * @Description: 新建文章，自动使用 Typora 打开
  */
 const spawn = require('child_process').exec;
-
-hexo.on('new', (data) => spawn('start "D:/Program Files/Typora.exe" ' + data.path));
+hexo.on('new', (data) => {
+  const isLinux = process.platform.includes('linux');
+  if (isLinux) {
+    spawn('typora ' + data.path);
+  } else {
+    spawn('start "D:/Program Files/Typora.exe" ' + data.path);
+  }
+});
